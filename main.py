1b209fbcc8dda9c1598491501d839fe59aabcb39
unknown <salunkhepriyanka6542@gmail.com>
2025-10-04 20:03:14 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from database.requirement import init_db, insert_job, fetch_jobs
 
 app = Flask(__name__)
@@ -9,24 +9,28 @@ init_db()
 def home():
     return render_template("home.html")
 
-@app.route("/add_requirement")
+@app.route("/add_requirement",methods=["GET","POST"])
 def add_requirement():
+    if request.method == "POST":
+        job_title = request.form["title"]
+        skills = request.form["skills"]
+        experience = request.form["experience"]
+
+        insert_job(job_title, skills, experience)
+
+        return render_template(
+            "success.html", 
+            title=job_title, 
+            skills=skills, 
+            experience=experience
+        )
+
     return render_template("requirement_form.html")
 
-@app.route("/submit_requirement",methods=['POST'])
-def submit():
-    job_title = request.form["title"]
-    skills = request.form["skills"]
-    experience = request.form["experience"]
-
-    insert_job(job_title,skills,experience)
-
-    return render_template("success.html",title=job_title,skills=skills,experience=experience)
-
-@app.route("/requirements")
+@app.route("/show_requirement",methods=["GET"])
 def show_requirements():
     jobs = fetch_jobs()
-    return render_template("requirments.html",jobs=jobs)
+    return render_template("requirements.html", jobs=jobs)
 
 
 if __name__=="__main__":
